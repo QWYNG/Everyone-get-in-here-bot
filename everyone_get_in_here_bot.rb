5930f8baf36a918142d53aed71af5cc611d0edbb
QWYNG <ikusawasi@gmail.com>
2018-12-10 22:06:50 +0900

    add no more same game notify but 2hour reset!

--- everyone_get_in_here_bot.rb
+++ everyone_get_in_here_bot.rb
@@ -5,14 +5,22 @@ TEXT_CHANNEL_TYPE_NUMBER = 0
 
 bot = Discordrb::Commands::CommandBot.new token: CONFIG['token']
 
+user_played_game_hash = {}
+
 bot.playing do |event|
-  if event.user.game
+  now_playng_game = event.user.game
+  user_name = event.user.name
+
+  if now_playng_game && now_playng_game != user_played_game_hash[user_name]
     first_text_channel = nil
     event.server.channels.each do |channel|
       first_text_channel ||= channel if channel.type == TEXT_CHANNEL_TYPE_NUMBER
       break if first_text_channel
     end
-    event.bot.send_message(first_text_channel, "#{event.user.name} playing #{event.user.game} now!")
+    event.bot.send_message(first_text_channel, "#{user_name} playing #{now_playng_game} now!")
+    user_played_game_hash[user_name] = now_playng_game
+    sleep(7200)
+    user_played_game_hash = {}
   end
 end
 
