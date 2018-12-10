880c1189b36600c0056c6ff6dd64bd7a9b9e8fa6
QWYNG <ikusawasi@gmail.com>
2018-12-10 23:06:46 +0900

    user_played_game_hash -> user_before_playing_game_hash for clear var and use before_playng_game for represent mean

--- everyone_get_in_here_bot.rb
+++ everyone_get_in_here_bot.rb
@@ -5,13 +5,15 @@ TEXT_CHANNEL_TYPE_NUMBER = 0
 TWO_HOUR_SEC = 7200
 
 bot = Discordrb::Commands::CommandBot.new token: CONFIG['token']
-user_played_game_hash = {}
+user_before_playing_game_hash = {}
 
 bot.playing do |event|
-  now_playng_game = event.user.game
   user_name = event.user.name
+  now_playng_game = event.user.game
+  before_playing_game = user_before_playing_game_hash[user_name]
 
-  if now_playng_game && now_playng_game != user_played_game_hash[user_name]
+  # ゲームをプレイしていてかつ前プレイしてるゲームと違うときのみ反応
+  if now_playng_game && now_playng_game != before_playing_game
     # 最初に見つけたテキストチャンネルをfirst_text_channelに代入
     first_text_channel = nil
     event.server.channels.each do |channel|
@@ -21,10 +23,10 @@ bot.playing do |event|
     # メッセージを送信
     event.bot.send_message(first_text_channel, "#{user_name} playing #{now_playng_game} now!")
     # プレイしてるゲームをuser_played_game_hashに記録
-    user_played_game_hash[user_name] = now_playng_game
+    user_before_playing_game_hash[user_name] = now_playng_game
     # 二時間後に記録はリセット
     sleep(TWO_HOUR_SEC)
-    user_played_game_hash.delete(user_name)
+    user_before_playing_game_hash[user_name].delete(user_name)
   end
 end
 
