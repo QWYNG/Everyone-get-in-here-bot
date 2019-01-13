8f81d836fc5459dac316d6d4c813a6812143d77b
QWYNG <ikusawasi@gmail.com>
2019-01-13 21:54:54 +0900

    add rspec and create get_first_text_channel

--- everyone_get_in_here_bot.rb
+++ everyone_get_in_here_bot.rb
@@ -7,21 +7,12 @@ TWO_HOUR_SEC = 7200
 bot = Discordrb::Commands::CommandBot.new token: CONFIG['token'], cliebt_id: CONFIG['client_id'], prefix: '!'
 user_before_playing_game_hash = {}
 
-def get_first_text_channel(channles)
-  first_text_channel = nil
-  channles.each do |channel|
-    first_text_channel ||= channel if channel.type == TEXT_CHANNEL_TYPE_NUMBER
-    break if first_text_channel
-  end
-  first_text_channel
-end
-
 bot.playing do |event|
   user_name = event.user.name
   now_playng_game = event.user.game
   before_playing_game = user_before_playing_game_hash[user_name]
 
-  if now_playng_game && now_playng_game != before_playing_game
+  if !now_playng_game == 'Spotify' && now_playng_game && now_playng_game != before_playing_game
     first_text_channel = get_first_text_channel(event.server.channels)
     event.bot.send_message(first_text_channel, "#{user_name} playing #{now_playng_game} now!")
     user_before_playing_game_hash[user_name] = now_playng_game
@@ -33,6 +24,15 @@ end
 
 bot.command :help do |event|
   event.respond("This is a bot that notifies the server that the user started the game on the server's first text channel")
+end
+
+def get_first_text_channel(channles)
+  first_text_channel = nil
+  channles.each do |channel|
+    first_text_channel ||= channel if channel.type == TEXT_CHANNEL_TYPE_NUMBER
+    break if first_text_channel
+  end
+  first_text_channel
 end
 
 bot.run
